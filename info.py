ed09b0605051f95dc9c79efadc9f23c93a346d12
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2023-02-12 15:09:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kcvk:kccinama@cluster0.0wdsl4s.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
